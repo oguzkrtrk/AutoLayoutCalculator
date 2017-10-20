53fad0204a935526f49b7a9299b1e557425c1f7b
Oğuz Karatoruk <oguzkaratoruk@yandex.com>
2017-10-20 23:23:48 +0300

    Initial Commit

---
+++ AutoLayoutCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoLayoutCalculator
+//
+//  Created by Oguz on 20.10.2017.
+//  Copyright © 2017 Oguz Karatoruk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
